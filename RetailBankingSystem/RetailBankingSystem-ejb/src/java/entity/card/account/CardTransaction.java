b788d85a4d7ef351265ef82ad587f2d4fb8a9935
Wang Zhe <wangzhe.lynx@gmail.com>
2016-10-01 01:45:12 +0800

    remove cascading persist on CardTransaction Entity

--- RetailBankingSystem/RetailBankingSystem-ejb/src/java/entity/card/account/CardTransaction.java
+++ RetailBankingSystem/RetailBankingSystem-ejb/src/java/entity/card/account/CardTransaction.java
@@ -44,7 +44,7 @@ public class CardTransaction implements Serializable {
     private boolean isCredit;
     private double amount;
     private CardTransactionStatus cardTransactionStatus;
-    @ManyToOne(cascade = {CascadeType.PERSIST})
+    @ManyToOne(cascade = {CascadeType.MERGE})
     private CreditCardAccount creditCardAccount;
     
 
