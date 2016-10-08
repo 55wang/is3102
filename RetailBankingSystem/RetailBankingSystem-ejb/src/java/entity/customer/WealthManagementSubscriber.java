e5cc017a3db69a5fc25fe71ace1f8fa3d26f7f9d
Wang Zhe <wangzhe.lynx@gmail.com>
2016-10-08 18:24:48 +0800

    change auto to identity and init to wealth page

--- RetailBankingSystem/RetailBankingSystem-ejb/src/java/entity/customer/WealthManagementSubscriber.java
+++ RetailBankingSystem/RetailBankingSystem-ejb/src/java/entity/customer/WealthManagementSubscriber.java
@@ -28,7 +28,7 @@ import server.utilities.EnumUtils.RiskToleranceLevel;
 public class WealthManagementSubscriber implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private Integer riskToleranceScore = null;
--- RetailBankingSystem/RetailBankingSystem-ejb/src/java/entity/wealth/Portfolio.java
+++ RetailBankingSystem/RetailBankingSystem-ejb/src/java/entity/wealth/Portfolio.java
@@ -23,7 +23,7 @@ import javax.persistence.OneToMany;
 public class Portfolio implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @OneToMany(cascade = CascadeType.MERGE, mappedBy = "portfolio")
