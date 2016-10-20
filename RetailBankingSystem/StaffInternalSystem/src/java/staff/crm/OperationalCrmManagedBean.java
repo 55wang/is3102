29b1759637ccc19c196eb43113c0cdecd4b0b2f5
Wang Zhe <wangzhe.lynx@gmail.com>
2016-10-21 00:04:34 +0800

    added some functions CRM

---
+++ RetailBankingSystem/StaffInternalSystem/src/java/staff/crm/OperationalCrmManagedBean.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package staff.crm;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author wang
+ */
+@Named(value = "operationalCrmManagedBean")
+@Dependent
+public class OperationalCrmManagedBean {
+
+    /**
+     * Creates a new instance of OperationalCrmManagedBean
+     */
+    public OperationalCrmManagedBean() {
+    }
+
+    public void viewEmailCampaign() {
+    }
+    
+    public void sendEmailCampaign() {
+    }
+
+}
